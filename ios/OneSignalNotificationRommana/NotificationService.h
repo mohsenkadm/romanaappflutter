08a654952a2e5ab8bbfb937320c10dd71ea33bcb
mohsenkadm <mohsenkadm123@gmail.com>
2024-01-05 00:49:10 +0300

    last

---
+++ ios/OneSignalNotificationRommana/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationRommana
+//
+//  Created by mohsen kadm on 14/12/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ ios/OneSignalRomana/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalRomana
+//
+//  Created by mohsen kadm on 06/11/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
